3c8923ce4a979afc63729ba92ee4b385f090dc29
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-09 14:33:39 +0530

    Add main.py
    Blank code is added.

---
+++ main.py
@@ -0,0 +1,12 @@
+###############################################################################
+# File          : main.py
+# Created by    : Rahul Kedia
+# Created on    : 09/01/2020
+# Project       : ReadOMR
+# Description   : This file contains the main source code for the project.
+################################################################################
+
+import cv2
+import numpu as np
+
+print("Code initialised")
