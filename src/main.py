7a11d6a969ba39114daf7fda03bc4845ba89cfe1
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-09 20:10:08 +0530

    Refactor HoughCircle method and Find Corner circles
    Problem in Hough Circle method is fixed by playing with parameter
    "dp". Now corner circles are also found. A simple logic applied
    for this is that the corner circles must be present in within
    90 pixel square from their respected corner.

--- src/main.py
+++ src/main.py
@@ -16,21 +16,47 @@ NewSize = (800, 800)
 InputImage = cv2.resize(InputImage, NewSize)
 
 # Extract the corner four circle's centre point
-gimg = cv2.cvtColor(InputImage, cv2.COLOR_BGR2GRAY)
-img = cv2.medianBlur(gimg,5)
-#cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+## Apply Hough Circle Detection
+GrayImage = cv2.cvtColor(InputImage, cv2.COLOR_BGR2GRAY)
+BlurImage = cv2.medianBlur(GrayImage, 7)
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=0,maxRadius=20)
+Circles = cv2.HoughCircles(BlurImage, cv2.HOUGH_GRADIENT, 1.3, 20,
+                            param1=50, param2=30, minRadius=9, maxRadius=20)
+Circles = np.uint16(np.around(Circles))
 
-circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
+
+## Filter circles according to their position. We are interested in corner circles only
+CornerCircles = np.zeros((4,3), np.uint16())
+NumOfCCFound = 0
+
+for i in Circles[0]:
+    if NumOfCCFound < 4:
+        if ((i[0] <= 90) & (i[1] <= 90)).all():
+            CornerCircles[NumOfCCFound] = i
+            NumOfCCFound += 1
+        elif ((i[0] <= 90) & (i[1] >= 710)).all():
+            CornerCircles[NumOfCCFound] = i
+            NumOfCCFound += 1
+        elif ((i[0] >= 710) & (i[1] <= 90)).all():
+            CornerCircles[NumOfCCFound] = i
+            NumOfCCFound += 1
+        elif ((i[0] >= 710) & (i[1] >= 710)).all():
+            CornerCircles[NumOfCCFound] = i
+            NumOfCCFound += 1
+    else:
+        break
+
+FinalCornerCircles = np.zeros((1, 4, 3), np.uint16())
+FinalCornerCircles[0] = CornerCircles
+
+
+for i in FinalCornerCircles[0, :]:
     # draw the outer circle
-    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(BlurImage, (i[0], i[1]), i[2], (0, 255, 0), 2)
     # draw the center of the circle
-    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+    cv2.circle(BlurImage, (i[0], i[1]), 2, (0, 0, 255), 3)
 
-cv2.imshow('detected circles',img)
+cv2.imshow('detected circles', BlurImage)
 
 
 
