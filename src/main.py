99810ee169b852475f862e60a391ecea89f00a22
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-09 20:42:14 +0530

    Refactor code
    Functions defined to make code modular and updated
    documentation

--- src/main.py
+++ src/main.py
@@ -15,55 +15,118 @@ InputImage = cv2.imread("InputImages/Blank.jpeg")
 NewSize = (800, 800)
 InputImage = cv2.resize(InputImage, NewSize)
 
-# Extract the corner four circle's centre point
-## Apply Hough Circle Detection
-GrayImage = cv2.cvtColor(InputImage, cv2.COLOR_BGR2GRAY)
-BlurImage = cv2.medianBlur(GrayImage, 7)
 
-Circles = cv2.HoughCircles(BlurImage, cv2.HOUGH_GRADIENT, 1.3, 20,
-                            param1=50, param2=30, minRadius=9, maxRadius=20)
-Circles = np.uint16(np.around(Circles))
+################################################################################
+# Function      : HoughCircleDetection
+# Parameter     : GrayImage - It contains gray scale image of Input Image
+#                 BlurImage - It contains blur image of GrayImage
+#                 Circles - It contains information of all the circles
+#                           detected by HoughCircles function.
+#                           Information is - [x-coordinate of centre,
+#                              y-coordinate of centre, radius of circle]
+# Description   : This function finds all the possible circles in the input
+#                 image by using HoughCircles algorithm.
+# Return        : Circles
+################################################################################
+def HoughCircleDetection():
+    GrayImage = cv2.cvtColor(InputImage, cv2.COLOR_BGR2GRAY)
+    BlurImage = cv2.medianBlur(GrayImage, 7)
 
+    Circles = cv2.HoughCircles(BlurImage, cv2.HOUGH_GRADIENT, 1.3, 20,
+                               param1=50, param2=30, minRadius=9, maxRadius=20)
+    Circles = np.uint16(np.around(Circles))
 
-## Filter circles according to their position. We are interested in corner circles only
-CornerCircles = np.zeros((4,3), np.uint16())
-NumOfCCFound = 0
-
-for i in Circles[0]:
-    if NumOfCCFound < 4:
-        if ((i[0] <= 90) & (i[1] <= 90)).all():
-            CornerCircles[NumOfCCFound] = i
-            NumOfCCFound += 1
-        elif ((i[0] <= 90) & (i[1] >= 710)).all():
-            CornerCircles[NumOfCCFound] = i
-            NumOfCCFound += 1
-        elif ((i[0] >= 710) & (i[1] <= 90)).all():
-            CornerCircles[NumOfCCFound] = i
-            NumOfCCFound += 1
-        elif ((i[0] >= 710) & (i[1] >= 710)).all():
-            CornerCircles[NumOfCCFound] = i
-            NumOfCCFound += 1
-    else:
-        break
-
-FinalCornerCircles = np.zeros((1, 4, 3), np.uint16())
-FinalCornerCircles[0] = CornerCircles
+    return Circles
 
 
-for i in FinalCornerCircles[0, :]:
-    # draw the outer circle
-    cv2.circle(BlurImage, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    cv2.circle(BlurImage, (i[0], i[1]), 2, (0, 0, 255), 3)
+################################################################################
+# Function      : FindCornerCircles
+# Parameter     : Circles - It contains information of all the circles
+#                           detected by HoughCircles function.
+#                           Information is - [x-coordinate of centre,
+#                              y-coordinate of centre, radius of circle]
+#                 NumOfCCFound - It keeps the count of number of centre
+#                                circles found in the image during
+#                                filtering of circles.
+#                 CornerCircles - It stores information of the corner
+#                                 circles detected.
+#                 FinalCornerCircles - It is a dummy variable used to add
+#                           a dimention in CornerCircles to make it similar
+#                           to the output circles array from HoughCircles algo.
+# Description   : This function finds corner circles from the array of all
+#                 the circles found by HoughCircleDetection.
+# Return        : FinalCornerCircles
+################################################################################
+def FindCornerCircles(Circles):
+    CornerCircles = np.zeros((4, 3), np.uint16())
+    NumOfCCFound = 0
 
-cv2.imshow('detected circles', BlurImage)
+    for i in Circles[0]:
+        if NumOfCCFound < 4:
+            if ((i[0] <= 90) & (i[1] <= 90)).all():
+                CornerCircles[NumOfCCFound] = i
+                NumOfCCFound += 1
+            elif ((i[0] <= 90) & (i[1] >= 710)).all():
+                CornerCircles[NumOfCCFound] = i
+                NumOfCCFound += 1
+            elif ((i[0] >= 710) & (i[1] <= 90)).all():
+                CornerCircles[NumOfCCFound] = i
+                NumOfCCFound += 1
+            elif ((i[0] >= 710) & (i[1] >= 710)).all():
+                CornerCircles[NumOfCCFound] = i
+                NumOfCCFound += 1
+        else:
+            break
+
+    FinalCornerCircles = np.zeros((1, 4, 3), np.uint16())
+    FinalCornerCircles[0] = CornerCircles
+
+    return FinalCornerCircles
 
 
+################################################################################
+# Function      : PrintCornerCircles
+# Parameter     : CornerCircles - It stores information of the corner
+#                                 circles detected.
+# Description   : This function prints the corner circles on the image.
+# Return        : FinalCornerCircles
+################################################################################
+def PrintCornerCircles(CornerCircles):
+    for i in CornerCircles[0, :]:
+        # draw the outer circle
+        cv2.circle(InputImage, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        # draw the center of the circle
+        cv2.circle(InputImage, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+    cv2.imshow('Detected Corner Circles', InputImage)
 
 
+################################################################################
+# Function      : CropReqOMR
+# Parameter     : Circles - It contains information of all the circles
+#                           detected by HoughCircles function.
+#                           Information is - [x-coordinate of centre,
+#                              y-coordinate of centre, radius of circle]
+#                 CornerCircles - It stores information of the corner
+#                                 circles detected.
+# Description   : This function calls suitable functions one by one for
+#                 detecting circles, and the rearranging/resizing the OMR
+#                 sheet so that then the answers can be found from OMR Sheet.
+# Return        : -
+################################################################################
+def CropReqOMR():
+    # Extract the corner four circle's centre point
+    ## Apply Hough Circle Detection
+    Circles = HoughCircleDetection()
+
+    ## Filter circles according to their position. We are interested in corner circles only
+    CornerCircles = FindCornerCircles(Circles)
+
+    ## Print corner circles
+    PrintCornerCircles(CornerCircles)
 
 
+# Crop the required OMR sheet for answer detection
+CropReqOMR()
 
-# Show output image
-cv2.imshow("Result", InputImage)
 cv2.waitKey(0)
