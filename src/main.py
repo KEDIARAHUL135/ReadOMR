14e986efc4465cb0114cced3a6784f35ef9f5f89
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-09 18:51:48 +0530

    Add HoughCircle method to find corner circles
    This method failed as it is not detecting two circles present at
    right hand side. Possible reason is because they are tilted. That
    is they are not prefectly circular in shape, they are elliptical.
    Other approach will be adopted now.

--- src/main.py
+++ src/main.py
@@ -9,4 +9,35 @@
 import cv2
 import numpy as np
 
-print("Code initialised")
+
+# Read Input and resize it
+InputImage = cv2.imread("InputImages/Blank.jpeg")
+NewSize = (800, 800)
+InputImage = cv2.resize(InputImage, NewSize)
+
+# Extract the corner four circle's centre point
+gimg = cv2.cvtColor(InputImage, cv2.COLOR_BGR2GRAY)
+img = cv2.medianBlur(gimg,5)
+#cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=20)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',img)
+
+
+
+
+
+
+
+# Show output image
+cv2.imshow("Result", InputImage)
+cv2.waitKey(0)
